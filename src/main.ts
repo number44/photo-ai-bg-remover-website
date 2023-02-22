2bd24eb4cae8a0c61686b31d91bf310ab27a3309
number44 <daniel.lesiewicz.dev@gmail.com>
2023-02-22 14:42:53 +0100

    x

--- src/main.ts
+++ src/main.ts
@@ -86,3 +86,17 @@ btnPrimary.forEach((btn) => {
     });
   });
 });
+const acc = document.getElementsByClassName(
+  "accordion"
+) as HTMLCollectionOf<HTMLElement>;
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function (this: HTMLElement) {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling as HTMLElement;
+    if (panel) {
+      panel.style.maxHeight = panel.style.maxHeight
+        ? null!
+        : `${panel.scrollHeight}px`;
+    }
+  });
+}
